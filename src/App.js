a827d13918bf8c70955023b71c5bfeec63df783c
unknown <msandoval@tolkogroup.com>
2023-01-28 01:39:46 +0100

    NEW FEATURE: sección de NOTES se crea y es responsive

--- src/App.js
+++ src/App.js
@@ -1,34 +1,54 @@
 //Components
-import Nav from './components/Nav'
-import Header from './components/Header';
-import { ImgCard } from './components/Cards';
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import { ImgCard, VidCard } from "./components/Cards";
 
 //Img
-import September from './assets/img/september.png';
-import PrRounded from './assets/img/prRounded.png';
-
+import September from "./assets/img/september.png";
+import PrRounded from "./assets/img/prRounded.png";
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      <Header/>
-      <section className='notes'>
-        <div className='notes__info'>
-          <ImgCard
-            imgSrc={September}
-            alt = "september"
-            title = "Lorem ipsum dolor sit amet"
-            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-          />
-          <ImgCard
-            imgSrc={PrRounded}
-            alt = "september"
-            title = "Lorem ipsum dolor sit amet"
-            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-          />
+      <Nav />
+      <Header />
+      <section className="notes cornerSpace">
+        <div className="notes__info">
+          <div className="notes__info--one">
+            <ImgCard
+              imgSrc={September}
+              alt="september"
+              title="Lorem ipsum dolor sit amet"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            />
+          </div>
+
+          <div className="notes__info--two">
+            <ImgCard
+              imgSrc={September}
+              alt="september"
+              title="Lorem ipsum dolor sit amet"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            />
+          </div>
         </div>
-        <div className='notes__videos'></div>
+        <div className="notes__videos">
+          <div className="notes__videos--rectangle"></div>
+          <div className="notes__videos--videos">
+            <div className="notes__videos--videosOne">
+              <VidCard
+                title="Lorem ipsum dolor sit amet"
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+              />
+            </div>
+            <div className="notes__videos--videosTwo">
+              <VidCard
+                title="Lorem ipsum dolor sit amet"
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+              />
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
