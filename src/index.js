f0b5c6a88aa5dfa661bbae81ce408908d58c3d89
unknown <msandoval@tolkogroup.com>
2023-01-29 16:29:20 +0100

    FIX: se borra invoación a i18next

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./assets/styles/scss/main.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import './assets/config/i18next_config' //Se invoca la configuración de i18next que realizamos
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
