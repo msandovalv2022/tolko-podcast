909d4b610108639e591dea0c3d14cf92d3a8ae14
unknown <msandoval@tolkogroup.com>
2023-01-30 12:46:47 +0100

    Se instala gsap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,8 @@
+// Hooks
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger' 
+import {useRef, useEffect} from 'react' // Es necesario importar estos 2 hooks para poder trabajar con gsap en react
+
 import Plane from "../assets/img/plane.png";
 import bannerSlogan from "../assets/img/bannerSlogan.png";
 
